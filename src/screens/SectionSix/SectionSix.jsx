f1af0a5bd538a78380aa77fe3d48363ac2dfbdcb
Belen <belen.c@polynomium.com>
2024-08-29 12:29:08 -0300

    video gif

--- src/screens/SectionSix/SectionSix.jsx
+++ src/screens/SectionSix/SectionSix.jsx
@@ -1,22 +1,21 @@
-
 import './styles.css';
 import ButtonComponent from '../../components/buttonComponent/buttonComponent';
 
-
 const SectionSix = () => {
-
+    const gifUrl = 'https://media.giphy.com/media/0xhORIXTg2XPQeoJQk/giphy.gif';
 
     return (
         <section className='sectionSix'>
+            <div className='containerVideosSix'>
             <div className='containerTextSix'>
                 <p className='itemSix'>ESTÁS LISTO?</p>
                 <h2 className='titleSectionSix'>COMENCEMOS</h2>
-                <p className='subtitleSectionSix'>Crea experiencias de cobro excepcionales potenciando el crecimiento de tu negocio con tu cuenta Megapix. </p>
+                <p className='subtitleSectionSix'>Crea experiencias de cobro excepcionales potenciando el crecimiento de tu negocio con tu cuenta Megapix.</p>
                 <ButtonComponent title='Ir a la plataforma' />
             </div>
-            <div className='containerVideosSix'>
+            
                 <div className='containerVideo'>
-                    
+                    <img src={gifUrl} alt='Example GIF' className='videoGif' />
                 </div>
             </div>
         </section>
