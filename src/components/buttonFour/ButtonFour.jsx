cc195681ee379460e247c4b2805c03c09d9dd9b7
Belen <belen.c@polynomium.com>
2024-09-02 10:49:28 -0300

    funcionalidades section carga de img

--- src/components/buttonFour/ButtonFour.jsx
+++ src/components/buttonFour/ButtonFour.jsx
@@ -2,20 +2,20 @@ import './styles.css';
 import PropTypes from 'prop-types';
 import Arrow from '../../images/arrow.png';
 
-const ButtonFour = ({ title }) => {
+const ButtonFour = ({ title, onClick }) => {
     return (
         <div className='containerButtonFour'>
-        <button className='buttonFour'>
-            <p className='textButtonFour'>{title}</p>
-            <img className='arrowBtn' src={Arrow} />
-        </button>
+            <button className='buttonFour' onClick={onClick}>
+                <p className='textButtonFour'>{title}</p>
+                <img className='arrowBtn' src={Arrow} alt='arrow' />
+            </button>
         </div>
-    )
+    );
 };
 
 ButtonFour.propTypes = {
     title: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
 };
-
 
 export default ButtonFour;
--- src/screens/SectionFour/SectionFour.jsx
+++ src/screens/SectionFour/SectionFour.jsx
@@ -1,31 +1,38 @@
-
+import { useState } from 'react';
 import Pix from '../../images/pix.png';
 import ButtonFour from '../../components/buttonFour/ButtonFour';
-import GrupoUno from '../../images/grupoUno.png';
+import ImgGroup from '../../images/grupoUno.png';
 import './styles.css';
 
 const SectionFour = () => {
+
+    const [activeImage, setActiveImage] = useState(ImgGroup);
+
+    const handleButtonClick = (image) => {
+        setActiveImage(image);
+    };
+
     return (
         <section className="sectionFour">
-            <img className='pixImage' src={Pix} />
+            <img className='pixImage' src={Pix} alt='pix' />
             <div className='cardFour'>
                 <h2 className='titleFour'>Funcionalidades principales</h2>
-              <div className='containerBtnFour'>
-                <ButtonFour title='Gestión de cobros diarios'/>
-                <ButtonFour title='Gestión de billetera'/>
-                <ButtonFour title='Gestión de ventas'/>
-                <ButtonFour title='Gestión de tiendas'/>
-                <ButtonFour title='Gestión de usuarios'/>
-              </div>
+                <div className='containerBtnFour'>
+                    <ButtonFour title='Gestión de cobros diarios' onClick={() => handleButtonClick(ImgGroup)} />
+                    <ButtonFour title='Gestión de billetera' onClick={() => handleButtonClick('')} />
+                    <ButtonFour title='Gestión de ventas' onClick={() => handleButtonClick('')} />
+                    <ButtonFour title='Gestión de tiendas' onClick={() => handleButtonClick('')} />
+                    <ButtonFour title='Gestión de usuarios' onClick={() => handleButtonClick('')} />
+                </div>
             </div>
             <div className='containerMoveImageFour'>
-              <div className='moveImage'>
-                <img className='imgGroup' src={GrupoUno} alt='funcionalidad' />
-              </div>
+                <div className='moveImage'>
+                    
+                    {activeImage && <img className='imgGroup' src={activeImage} alt='funcionalidad' />}
+                </div>
             </div>
-
         </section>
-    )
+    );
 };
 
 export default SectionFour;
