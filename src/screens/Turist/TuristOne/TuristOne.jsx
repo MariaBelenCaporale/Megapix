984614f237ddaa45f859a8d09b91f1f357ffd629
Belen <belen.c@polynomium.com>
2024-09-03 18:20:03 -0300

    phone carousel

--- src/screens/Turist/TuristOne/TuristOne.jsx
+++ src/screens/Turist/TuristOne/TuristOne.jsx
@@ -6,33 +6,18 @@ import Iguazu from '../../../images/iguazu.png';
 import Ski from '../../../images/ski.png';
 import Mdz from '../../../images/mdz.png';
 
+const images = [Ushuaia, Mendoza, Bariloche, Iguazu, Ski, Mdz];
 
 const TuristOne = () => {
-    
-
     return (
         <section className="turistOneContainer">
-            <div className='containerCarousel'>
-                <div className="containerPlaces">
-                    <img className='imgPhoneOneTurist' src={Ushuaia} alt='Imagen de Ushuaia' />
-                </div>
-                <div className="containerPlaces">
-                    <img className='imgPhoneOneTurist' src={Mendoza} alt='Imagen de Mendoza' />
-                </div>
-                <div className="containerPlaces">
-                    <img src={Ushuaia} alt='Imagen de Ushuaia' />
-                </div>
-                <div className="containerPlaces">
-                    <img className='imgPhoneOneTurist' src={Bariloche} alt='Imagen de Bariloche' />
-                </div>
-                <div className="containerPlaces">
-                    <img  className='imgPhoneOneTurist'src={Iguazu} alt='Imagen de Iguazu' />
-                </div>
-                <div className="containerPlaces">
-                    <img  className='imgPhoneOneTurist'src={Ski} alt='Imagen de persona haciendo ski' />
-                </div>
-                <div className="containerPlaces">
-                    <img  className='imgPhoneOneTurist'src={Mdz} alt='Imagen de persona haciendo ski' />
+            <div className="containerCarousel">
+                <div className="carouselInner">
+                    {images.concat(images).map((src, index) => (
+                        <div className="containerPlaces" key={index}>
+                            <img className='imgPhoneOneTurist' src={src} alt={`Imagen ${index}`} />
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
