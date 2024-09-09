884939bac94aa372db565bad3810273875c68ca8
Belen <belen.c@polynomium.com>
2024-09-09 11:47:40 -0300

    pills responsive

--- src/screens/SectionTwo/SectionTwo.jsx
+++ src/screens/SectionTwo/SectionTwo.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
 import "./styles.css";
 
 const SectionTwo = () => {
+  const [selectedButton, setSelectedButton] = useState(null)
+
+  const handleButtonClick = (index) => {
+    setSelectedButton(index);
+  };
+  const buttons = [
+    "Control de tus finanzas",
+    "Cobros con QR",
+    "Conexión con turistas",
+    "Gratis",
+    "Multiplataforma",
+    "Rápido",
+    "Fácil",
+    "Sin comisiones",
+    "Seguridad",
+    "Tecnología PIX",
+    "Transacciones sin posnet",
+    "Cobros sin barreras",
+    "Segura",
+    "Soporte 24/7"
+  ];
+
   return (
     <section className="containerSectionTwo">
       <div className="blurTwo">
@@ -10,21 +33,15 @@ const SectionTwo = () => {
       </div>
       <div className="prueba" >
         <div className="rotated-items-container">
-          <button className="animation-1 animation">Control de tus finanzas</button>
-          <button className="animation-2 animation">Cobros con QR</button>
-          <button className="animation-3 animation">Conexión con turistas</button>
-          <button className="animation-4 animation">Gratis</button>
-          <button className="animation-5 animation">Multiplataforma</button>
-          <button className="animation-6 animation">Rápido</button>
-          <button className="animation-7 animation">Fácil</button>
-          <button className="animation-8 animation">Sin comisiones</button>
-          <button className="animation-9 animation">Seguridad</button>
-          <button className="animation-10 animation">Tecnología PIX</button>
-          <button className="animation-11 animation">Transacciones sin posnet</button>
-          <button className="animation-12 animation">Cobros sin barreras</button>
-          <button className="animation-13 animation">Segura</button>
-          <button className="animation-14 animation">Soporte 24/7</button>
-
+        {buttons.map((text, index) => (
+            <button
+              key={index}
+              className={`animation animation-${index + 1} ${selectedButton === index ? 'selected' : ''}`}
+              onClick={() => handleButtonClick(index)}
+            >
+              {text}
+            </button>
+          ))}
         </div>
       </div>
     </section>
