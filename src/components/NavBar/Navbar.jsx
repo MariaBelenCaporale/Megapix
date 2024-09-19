6790a64878a878466a18458ecd2b8f8923a025b6
Belen <belen.c@polynomium.com>
2024-09-19 11:45:50 -0300

    changes switch language navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,20 +1,20 @@
-import './styles.css';
-import Logo from '@images/logoNeon.png';
-import SwitchComponent from '../Switch/SwitchComponent';
-import LanguageSelect from '../Languaje/LanguageSelect';
+import "./styles.css";
+import Logo from "@images/logoNeon.png";
+import SwitchComponent from "../Switch/SwitchComponent";
+import LanguageSelect from "../Languaje/LanguageSelect";
 
 const NavBar = () => {
-    return (
-        <div className='containerNav'>
-            <div className='containerLogo'>
-            <a className='linkLogoNav' href="#top">
-                    <img className='logo' src={Logo} alt="Logo Megapix" />
-                </a>
-            </div>
-            <SwitchComponent />
-            <LanguageSelect />
-        </div>
-    );
+  return (
+    <div className="containerNav">
+      <div className="containerLogo">
+        <a className="linkLogoNav" href="#top">
+          <img className="logo" src={Logo} alt="Logo Megapix" />
+        </a>
+      </div>
+      <SwitchComponent />
+      <LanguageSelect />
+    </div>
+  );
 };
 
 export default NavBar;
