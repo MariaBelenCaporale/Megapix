6270d27375ba53f95323b6b239abc9e8cdf2942d
Belen <belen.c@polynomium.com>
2024-10-09 18:16:02 -0300

    terminos

--- src/screens/Condiciones/Condiciones.jsx
+++ src/screens/Condiciones/Condiciones.jsx
@@ -1,44 +1,88 @@
-import CardTerminos from "../../components/cardTerminos/CardTerminos";
-import Footer from "../../components/Footer/Footer";
-import Terminos from "../Terminos/Terminos";
-import Logo from '@images/logoNeon.png';
+import { useState } from 'react';
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import { useTranslation } from 'react-i18next';
 import LanguageSelect from "../../components/Languaje/LanguageSelect";
+import Logo from '@images/logoNeon.png';
 import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded';
-import "./styles.css";
 import { NavLink } from "react-router-dom";
+import './styles.css';
+import Footer from '../../components/Footer/Footer';
+import Terminos from '../Terminos/Terminos';
 
 const Condiciones = () => {
-  return (
-    <section className="containerSectionCond">
-        <div className="containerNavCondiciones">
-      <div className="containerLogo">
-        <a className="linkLogoNav" href="/">
-          <img className="logoCon" src={Logo} alt="Logo Megapix" />
-        </a>
-      </div>
-      <LanguageSelect />
-    </div>
-    
-      <div className="containerTotalCond">
-        <CardTerminos />
-        <div className="containerTotalTerminos">
-        <div>
-            <NavLink to="/" className='return' >
-            <KeyboardArrowLeftRoundedIcon />
-            Volver
-            </NavLink>
-        </div>
-        <div className="containerTitleTerms">
-              <h2 className="titleTerms">Términos y condiciones</h2>
+    const { t } = useTranslation();
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const handleToggle = (index) => {
+        setActiveIndex(activeIndex === index ? null : index);
+    };
+
+    const faqs = [
+      {
+          question: t('Términos y condiciones'),
+          answer: <Terminos section="condiciones" />,
+      },
+      {
+          question: t("Políticas de privacidad"),
+          answer: <Terminos section="privacidad" />,
+      },
+      {
+          question: t("Términos y condiciones adicionales"),
+          answer: <Terminos section="adicionales" />,
+      },
+  ];
+
+    return (
+        <section className="containerSectionCond">
+          <div className="containerNavCondiciones">
+            <div className="containerLogo">
+              <a className="linkLogoNav" href="/">
+                <img className="logoCon" src={Logo} alt="Logo Megapix" />
+              </a>
             </div>
-        <div className="terminos">
-          <Terminos />
+              <LanguageSelect />
+          </div>
+          <div className='contenedorCondicionesTotal'>
+          <div>
+             <NavLink to="/" className='return' >
+             <KeyboardArrowLeftRoundedIcon />
+             Volver
+             </NavLink>
+         </div>
+         
+        <div className="containerTitleTerms">
+            <h2 className="titleTerms">Términos y condiciones</h2>
         </div>
-       </div>
-      </div>
-      <Footer />
-    </section>
-  );
-};
+
+        <div className='containerAsk'>
+                {faqs.map((faq, index) => (
+                    <div key={index} className='askCondiciones'>
+                        <div
+                            className='question'
+                            onClick={() => handleToggle(index)}
+                        >
+                            <p className={`questionText ${activeIndex === index ? 'active' : ''}`}>
+                                {faq.question}
+                            </p>
+                            
+                            <span className={`icon ${activeIndex === index ? 'open' : ''}`}>
+                                <KeyboardArrowDownRoundedIcon />
+                            </span>
+                        </div>
+                        <div className='line'> </div>
+                        {activeIndex === index && (
+                            <div className='response'>
+                                <p className='responseText'>{faq.answer}</p>
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+            </div>
+            <Footer />
+        </section>
+    );
+}
 
 export default Condiciones;
+
