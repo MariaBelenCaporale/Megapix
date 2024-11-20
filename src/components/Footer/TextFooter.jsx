334b7b2d880d9137968192ad5116d9fa09e770d0
Belen <belen.c@polynomium.com>
2024-11-20 17:27:50 -0300

    se agrego el texto debajo del footer

--- src/components/Footer/TextFooter.jsx
+++ src/components/Footer/TextFooter.jsx
@@ -7,7 +7,7 @@ const TextFooter = () => {
     return (
         <div className='containerTextImportant'>
             <div className='containerImportant'>
-            <p className='textImportant'>{t('Cavaliere Group S.R.L”- Proveedor de Servicios de Activos Virtuales (PSAV) inscripto bajo el N°X de fecha "X" en el Registro de Proveedores de Servicios de Activos Virtuales de CNV. Este registro es a los fines del control como Sujeto Obligado ante la Unidad de Información Financiera (UIF) y de todo otro ente regulador facultado a tal efecto, en el marco de sus competencias, y no implica licencia ni supervisión por parte de la COMISIÓN NACIONAL DE VALORES sobre la actividad realizada por el PSAV')}</p>
+            <p className='textImportant'>{t('Cavaliere Corporate Group S.R.L. - Proveedor de Servicios de Activos Virtuales (PSAV) inscripto bajo el N° 91 de fecha 20 de Noviembre de 2024 en el Registro de Proveedores de Servicios de Activos Virtuales de CNV. Este registro es a los fines del control como Sujeto Obligado ante la Unidad de Información Financiera (UIF) y de todo otro ente regulador facultado a tal efecto, en el marco de sus competencias, y no implica licencia ni supervisión por parte de la COMISIÓN NACIONAL DE VALORES sobre la actividad realizada por el PSAV')}</p>
             </div>
         </div>
     )
