4f341f651898677964a5a0183eeaa3456397ac08
Belen <belen.c@polynomium.com>
2024-12-09 17:36:16 -0300

    cambios

--- src/components/prensaComponent/PrensaComponent.jsx
+++ src/components/prensaComponent/PrensaComponent.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router-dom";
 import "./styles.css";
-import CampaignRoundedIcon from "@mui/icons-material/CampaignRounded";
+
 
 const PrensaComponent = () => {
 
   return (
     <NavLink to="/prensa" className="prensaIcon">
-      <CampaignRoundedIcon />
+      <p className="prensaTextNav">Prensa</p>
     </NavLink>
   );
 };
